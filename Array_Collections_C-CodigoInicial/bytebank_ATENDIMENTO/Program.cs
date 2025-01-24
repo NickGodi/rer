90cdb50a20d966d8f9eee162aa6c948aa54a90b4
Nick <nicolas.100gamas@gmail.com>
2025-01-24 04:12:10 -0300

    projeto iniciado

---
+++ Array_Collections_C-CodigoInicial/bytebank_ATENDIMENTO/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+
+//numero inteiro
+int.idade0 = 30;
+int.idade1 = 40;
+int.idade2 = 17;
+int.idade3 = 21;
+int.idade4 = 18;
+
+int media = (idade0 + idade1 + idade2 + idade3 + idade4) / 5;
+int.idade
---
+++ C/Forca.c
@@ -0,0 +1,6 @@
+int main () {
+// char = guardar letra
+    char letra1 = "m";
+    char letra2 = "m";
+    printf("%c", letra1);
+}
